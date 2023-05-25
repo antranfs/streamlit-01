2b46ade1982f9c0972bdfe2abbfeca036bddae22
An Tran <an.tran@fundingsocieties.com>
2023-05-25 15:21:55 +0700

    add sample

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,44 @@
+# Bring in deps
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
 import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
-st.title('🎈 App Name')
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
-st.write('Hello world!')
+# App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here')
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'],
+    template='you are a spend analyzer {topic}'
+)
+
+# Memory
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+
+
+# Llms
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    title = title_chain.run(prompt)
+
+    st.write(title)
+
+    with st.expander('Title History'):
+        st.info(title_memory.buffer)
